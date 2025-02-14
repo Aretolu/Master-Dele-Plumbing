677975998927a6f7c2368192ca9ed5b71a19531e
Kenny_dev <145924339+Aretolu@users.noreply.github.com>
2025-02-14 19:14:02 +0100

    Add files via upload

---
+++ Dele P/assets/js/app.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+// Close menu when clicking a link
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
+});
